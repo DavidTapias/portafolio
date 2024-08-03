91ef50c301dd91f687a21862957cd43e63ff537c
David Tapias <davidtapias0110@gmail.com>
2024-08-02 22:27:42 -0500

    Cambio a dominio personalizado

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://davidtapias.github.io',
-  base:'portafolio',
+  site: 'https://www.davidtapias.com',
   integrations: [tailwind()]
 });
