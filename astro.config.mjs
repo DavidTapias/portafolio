2b2c17407efc5e5bd9362549c82fb75dd3583473
David Tapias <davidtapias0110@gmail.com>
2024-08-02 21:21:53 -0500

    base

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://davidtapias.github.io',
-  base:'/portafolio',
+  base:'portafolio',
   integrations: [tailwind()]
 });
